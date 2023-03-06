69c180216b0d39616b0f46b43402d68b8624d5c5
Leo Generali <leo.d.generali@gmail.com>
2023-03-06 13:32:38 -0500

    Add text area and submit button

---
+++ app/constants/styles.ts
@@ -0,0 +1,1 @@
+export const CONTAINER = "max-w-[640px] mb-4 px-4 md:mb-8 w-full m-auto";
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import { Body } from "@leafygreen-ui/typography";
+import Footer from "@/components/footer";
+import { CONTAINER } from "@/constants/styles";
 
 export default function Home() {
   return (
@@ -9,8 +11,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <Body>Hello world!</Body>
+      <main className="flex flex-col min-h-screen">
+        <header className="h-10 bg-red-500">Header</header>
+        <div className={`${CONTAINER} flex-grow`}></div>
+        <Footer
+          onSubmit={() => {
+            console.log("Hello world!");
+          }}
+        />
       </main>
     </>
   );
