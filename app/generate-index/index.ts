dd4c71ba877b3a1811dc348821ed1d9485fdc9e0
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2023-03-06 18:11:16 -0500

    add few indexed sites

--- app/generate-index/index.ts
+++ app/generate-index/index.ts
@@ -9,10 +9,32 @@ const sitesToIndex = [
   {
     name: "app-services",
     url: "https://www.mongodb.com/docs/atlas/app-services/sitemap.xml",
+    siteType: "snooty",
   },
   {
     name: "realm",
     url: "https://www.mongodb.com/docs/realm/sitemap.xml",
+    siteType: "snooty",
+  },
+  {
+    name: "manual",
+    url: "https://www.mongodb.com/docs/manual/sitemap.xml",
+    siteType: "snooty",
+  },
+  {
+    name: "atlas",
+    url: "https://www.mongodb.com/docs/atlas/sitemap.xml",
+    siteType: "snooty",
+  },
+  {
+    name: "node-driver",
+    url: "https://www.mongodb.com/docs/drivers/node/current/sitemap.xml",
+    siteType: "snooty",
+  },
+  {
+    name: "go-driver",
+    url: "https://www.mongodb.com/docs/drivers/go/current/sitemap.xml",
+    siteType: "snooty",
   },
 ];
 
@@ -22,7 +44,7 @@ async function run() {
     const siteData = await genIndex(site.url, {
       writeToFile: `${site.name}.json`,
       maxTokens: 1000,
-      embeddingModel: process.env.EMBEDDING_MODEL as string,
+      embeddingModel: process.env.OPENAI_EMBEDDING_MODEL as string,
     });
     dbDocs.push(...siteData);
   }
