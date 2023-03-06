56bbb06f96d85dfa5475482799449ca6ba82f542
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2023-03-06 09:40:51 -0500

    test if preview builds

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -64,7 +64,7 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
+              Testing! <span>-&gt;</span>
             </h2>
             <p className={inter.className}>
               Find in-depth information about Next.js features and&nbsp;API.
