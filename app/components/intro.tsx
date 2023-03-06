76e8f248ab17e98857ddc9209ed0fd0075b76408
Leo Generali <leo.d.generali@gmail.com>
2023-03-06 16:12:41 -0500

    Make cheeky intro responsive

--- app/components/intro.tsx
+++ app/components/intro.tsx
@@ -2,10 +2,12 @@ import { H2, Body } from "@leafygreen-ui/typography";
 
 export default function Intro() {
   return (
-    <div class="grid grid-cols-[200px,1fr] items-center">
-      <div className="text-[150px] text-center">🧙</div>
+    <div class="flex items-center gap-4">
+      <div className="text-[60px] md:text-[150px] text-center">🧙</div>
       <div>
-        <H2 as="h1">Hey there! I'm the Oracle!</H2>
+        <H2 className="text-[20px] md:text-[32px]" as="h1">
+          Hey there! I'm the Oracle!
+        </H2>
         <Body className="text-[16px] mt-2">
           I'm a MongoDB Atlas Wizard. I've spent eons pouring over the
           documentation. Ask me how to do anything in Atlas - I'll do my best to
