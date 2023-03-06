29c6f4c0d7bd19fac3fad98ca27b0139d5105a8f
Leo Generali <leo.d.generali@gmail.com>
2023-03-06 16:12:41 -0500

    Make cheeky intro responsive

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -16,10 +16,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex flex-col min-h-screen">
-        <header>
+      <main className="flex flex-col min-h-screen pt-4">
+        <header className={`${CONTAINER}`}>
           <Logo
-            className="absolute top-6 left-6"
+            className="md:absolute top-6 left-6"
             height={40}
             name="MongoDBLogo"
           />
