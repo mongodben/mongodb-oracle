c2e5679f743b58a07047548d7e4a1cba59268894
Leo Generali <leo.d.generali@gmail.com>
2023-03-06 15:55:18 -0500

    Fetch from local api

--- app/pages/api/ask.ts
+++ app/pages/api/ask.ts
@@ -4,9 +4,17 @@ type Data = {
   answer: string;
 };
 
+function randomWaitTime() {
+  return Math.floor(Math.random() * (4_000 - 500) + 500); // The maximum is exclusive and the minimum is inclusive
+}
+
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ answer: "John Doe" });
+  setTimeout(() => {
+    res.status(200).json({
+      answer: "I am the oracle! This is coming from the NextJS API route",
+    });
+  }, randomWaitTime());
 }
