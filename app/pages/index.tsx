86baf92b512ad4d4133523c298f28e10a5ee3dd6
Leo Generali <leo.d.generali@gmail.com>
2023-03-07 17:13:40 -0500

    Add background; make copy disapear

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Image from "next/image";
-import { motion } from "framer-motion";
 
 import { CONTAINER } from "@/styles/constants";
 
@@ -8,7 +7,6 @@ import useMongoDBOracle from "@/hooks/use-mongodb-oracle";
 
 import { H2, Body } from "@leafygreen-ui/typography";
 
-import Oracle from "@/components/oracle";
 import Footer from "@/components/footer";
 import Message from "@/components/message";
 import Loader from "@/components/loader";
@@ -26,7 +24,7 @@ export default function Home() {
       </Head>
       <main className="flex flex-col h-screen pt-4">
         <header className={`${CONTAINER} flex-row`}>
-          <div className="md:relative top-6 pb-10">
+          <div className="w-[400px] absolute left-8 top-8">
             <Image
               src="/logo/logo-long-cropped.png"
               height={110}
@@ -35,8 +33,19 @@ export default function Home() {
             />
           </div>
 
-          <div className="flex items-center gap-4">
-            <Oracle />
+          <div className="relative flex items-center justify-center animate-[floating_6s_cubic-bezier(.76,0,.24,1)_infinite] select-none">
+            <div className="text-[140px]">🧙</div>
+            <div className="absolute bottom-0 text-[70px]">🔮</div>
+            <div className="absolute bottom-0 text-[40px] -translate-x-[20px] -translate-y-[25px] rotate-[20deg]">
+              🫱
+            </div>
+            <div className="absolute bottom-0 text-[40px] translate-x-[20px] -translate-y-[25px] rotate-[-20deg]">
+              🫲
+            </div>
+          </div>
+
+          {/* <Oracle /> */}
+          {status === "first-load" && (
             <div>
               <H2 className="text-[20px] md:text-[32px]" as="h1">
                 Hey there, I&apos;m the Oracle!
@@ -47,12 +56,14 @@ export default function Home() {
                 to help you on your quest.
               </Body>
             </div>
-          </div>
+          )}
         </header>
 
         <hr className="px-4 max-w-[320px] border-2 mx-auto w-full rounded border-lg-green-dark2 my-4" />
 
-        <div className={`${CONTAINER} flex-grow overflow-y-scroll h-[500px]`}>
+        <div
+          className={`${CONTAINER} flex-grow overflow-y-scroll py-4 bg-lg-gray-dark1/5 rounded-lg`}
+        >
           {messages.length > 0 && (
             <ul className="flex-grow space-y-2 flex flex-col overflow-hidden">
               {messages.map(({ id, type, children }) => (
