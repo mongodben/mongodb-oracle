4c66887d21498320c8fde453b12599b0110230fa
Nick Larew <nick.larew@mongodb.com>
2023-03-08 14:01:00 -0500

    remove keys

--- app/pusher/client.ts
+++ app/pusher/client.ts
@@ -3,10 +3,8 @@ import Pusher from "pusher-js";
 // Enable pusher logging - don't include this in production
 Pusher.logToConsole = false;
 
-const PUSHER_APP_KEY = process.env.PUSHER_APP_KEY ?? "08d4594463bfc786715f";
-console.log("pusher key", PUSHER_APP_KEY)
+const PUSHER_APP_KEY = process.env.PUSHER_APP_KEY;
 if (!PUSHER_APP_KEY) {
-  console.log("no pusher key", PUSHER_APP_KEY)
   throw new Error(
     `throw new Error('Invalid/Missing environment variable: "PUSHER_APP_KEY"');`
   );
